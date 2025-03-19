f5bfacd860605b5e72e379ab0268fa4da7a3eb10
lucabaz98 <luca.bazzetto@gmail.com>
2025-03-19 11:43:06 +0100

    cards version finalized

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Smooth scrolling for navigation links with Bootstrap offset adjustment
+// Smooth Scrolling for Navigation Links
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function(e) {
       e.preventDefault();
       const target = document.querySelector(this.getAttribute('href'));
       window.scrollTo({
         top: target.offsetTop - document.querySelector('.navbar').offsetHeight,
-        behavior: 'smooth'
+        behavior:'smooth'
       });
-      
-      // If on mobile, collapse the navbar after clicking
-      const navbarCollapse = document.querySelector('.navbar-collapse');
-      if (navbarCollapse.classList.contains('show')) {
-        new bootstrap.Collapse(navbarCollapse).toggle();
-      }
     });
-  });
-  
+   });
+   
+   // Initialize AOS (Animate on Scroll)
+   AOS.init();
+   
